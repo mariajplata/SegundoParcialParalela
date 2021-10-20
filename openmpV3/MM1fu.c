bb089c2179463a7dedea09298df2d1d2034746bf
mariajplata <maria.plata01@correo.usa.edu.co>
2021-10-19 21:48:26 -0500

    Corrección archivo MM1fu.c

--- openmpV3/MM1fu.c
+++ openmpV3/MM1fu.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include "funcionesmatrices.h"
 #include <omp.h>
 #include "ctime.h"
@@ -9,73 +8,64 @@
 
 static double MEM_CHUNK[SIZE_DATA];
 
-//Nueva version del algoritmo multiplicacion de matrices 2 filas * 2 filas
+// Nueva versión del algoritmo multiplicación de matrices 2 filas * 2 filas
 int N, numThreads;
 double *matrizA, *matrizB, *matrizC;
 
 int main(int argc, char **argv) {
 
-    int N = (int) atof(argv[1]); //tamaño de la amtriz
+    int N = (int) atof(argv[1]); //tamaño de la matriz
     int numThreads = (int) atof(argv[2]); // Cantidad de hilos
 
-//#pragma omp parallel{
-	int i, j, k;
-    double *matrizA, *matrizB, *matrizC;
-	matrizA = MEM_CHUNK;
-	matrizB = matrizA + (N*N);
-	matrizC = matrizB + (N*N);
+//	#pragma omp parallel 
+	{
+		int i, j, k;
+		double *matrizA, *matrizB, *matrizC;
+		matrizA = MEM_CHUNK;
+		matrizB = matrizA + (N*N);
+		matrizC = matrizB + (N*N);
 
-	//Se deja que le hilo principal(maestro) haga las rutinas de inicialización
-//#pragma omp master //2,4,8	
+		//Se deja que le hilo principal(maestro) haga las rutinas de inicialización
+//		#pragma omp master //2,4,8	
 
-	matrizInit(N, matrizA, matrizB, matrizC);
-	/*printf("Impresion matriz A \n");
-	imprimirMatriz(N,matrizA);
-	printf("Impresion matriz B \n");
-	imprimirMatriz(N,matrizB);*/
+		matrizInit(N, matrizA, matrizB, matrizC);
+		/*printf("Impresion matriz A \n");
+		imprimirMatriz(N,matrizA);
+		printf("Impresion matriz B \n");
+		imprimirMatriz(N,matrizB);*/
 
-	omp_set_num_threads (numThreads);
+		omp_set_num_threads (numThreads);
 
-	sampleStart();
-	//Prueba de multiplicacion de matrices de OpenMp
+		sampleStart();
+		// Prueba de multiplicación de matrices con OpenMp
 
-	#pragma omp parallel for
-		for (i=0; i<N; i++){
-			for (j=0; j<N; j++){
+		#pragma omp parallel for
+		for (i=0; i<N; i++) {
+			for (j=0; j<N; j++) {
 				double *pA, *pB, c0, c1, c2, c3;
 				c0 = c1 = c2 = c3 = 0.0;
-				pA = matrizA+(i*N); //Fila
-				pB = matrizB+(j*N); //Columna
+				pA = matrizA+(i*N); // Fila
+				pB = matrizB+(j*N); // Columna
 				k = N;
-
-				while(k&3){ //En caso de que N no sea multiplo de 4
+				while (k&3) {// En caso de que N no sea múltiplo de 4
 					c0 += (*pA * *pB);
 					k--;
 					pA++;
 					pB++;
 				}
-				
-				for (; k < 0; k-=4, pA+=4, pB+=4) {					
+				for (; k > 0; k-=4, pA+=4, pB+=4) {
 					c0 += (*pA * *pB);
-					c1 += (*(pA+1) * *(pB+1));
-					c2 += (*(pA+2) * *(pB+2));
-					c3 += (*(pA+3) * *(pB+3));
+					c1 += (*(pA + 1) * *(pB + 1));
+					c2 += (*(pA + 2) * *(pB + 2));
+					c3 += (*(pA + 3) * *(pB + 3));
 				}
-				
-				matrizC[i*N + j] = c0 + c1 + c2 +c3;
+
+				matrizC[i * N + j] = c0 + c1 + c2 + c3;
 			}
 		}
 		/*printf("impresion matriz C \n");
 		imprimirMatriz(N,matrizC);*/
 		sampleStop();
-		printTime();
-
+	}
+	printTime();
 }
-
-
-
-
-	
-
-
-
